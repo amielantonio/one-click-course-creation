ed709271a6a7f3507f673fb3e7609061e34c3311
Amiel Antonio <amielnicole.antonio@gmail.com>
2020-11-13 11:40:18 +0800

    Testing out create_sfwd_lesson() method

--- lib/Controller/ClassroomController.php
+++ lib/Controller/ClassroomController.php
@@ -37,72 +37,92 @@ class ClassroomController extends CoreController
      */
     public function store(Request $request)
     {
-        var_dump(get_post_meta(25632, '_sfwd-lessons'));
+//        $arrLessons = [];
+//
+        $dates = $request->input('topic-date');
+//        $lessonNames = $request->input('lesson-name');
+        $lessonIds = $request->input('lesson-id');
+//
+//        $lessonName = $request->input('lesson-name');
+//        $lessonID = $request->input('lesson-id');
+//
+//        //Get Dolly
+//        $dolly = new Posts;
+//        $dolly->find($request->input('course-content'));
+//
+//        //Cloning process for the course
+//        $course = new Posts;
+//        $course->post_title = $request->input('course-title');
+//        $course->post_author = get_current_user_id();
+//        $course->post_content = $dolly->post_content;
+//        $course->post_excerpt = $dolly->post_excerpt;
+//        $course->post_status = 'publish';
+//        $course->post_type = $dolly->post_type;
+//
+//        //Save to database
+//
+//        if ($course_id = wp_insert_post($course->get_columns())) {
+//            echo "{$request->input('course-title')} course created<br />";
+//
+//            // Post meta for the course
+//            $this->save_course_meta($course_id, $request, $dolly);
+//
+//            // Once the main course is saved, process the lesson
+//            for ($i = 0; $i < count($lessonName); $i++) {
+//                // Save the lessons to swfd-lesson post type
+//                $lesson = new Posts;
+//                $dollyLesson = new Posts;
+//                $dollyLesson->find($lessonID[$i]);
+//
+//                $lesson->post_title = $lessonName[$i];
+//                $lesson->post_author = get_current_user_id();
+//                $lesson->post_content = $dollyLesson->post_content;
+//                $lesson->post_excerpt = $dollyLesson->post_excerpt;
+//                $lesson->post_status = "publish";
+//                $lesson->post_type = $dollyLesson->post_type;
+//
+//                //Save Lesson to database
+//                if ( $arrLessons[] = $lesson_id = wp_insert_post($lesson->get_columns()) ) {
+//                    //Post meta
+//
+//                    //Create ld_course_steps meta
+//                    $this->save_lesson_meta($lesson_id, $course_id, $request, $dollyLesson);
+//
 
-        $arrLessons = [];
 
-        $lessonName = $request->input('lesson-name');
-        $lessonID = $request->input('lesson-id');
+                    $new_lesson_meta = [
+                        "sfwd-lessons_visible_after_specific_date" => Carbon::createFromFormat('d M Y, g:i a', $dates[0])->format('Y-m-d g:i a')
+                    ];
 
-        //Get Dolly
-        $dolly = new Posts;
-        $dolly->find($request->input('course-content'));
+//                    echo Carbon::createFromFormat('d M Y, g:i a', $dates[0])->format('Y-m-d g:i a') . "<br />";
 
-        //Cloning process for the course
-        $course = new Posts;
-        $course->post_title = $request->input('course-title');
-        $course->post_author = get_current_user_id();
-        $course->post_content = $dolly->post_content;
-        $course->post_excerpt = $dolly->post_excerpt;
-        $course->post_status = 'publish';
-        $course->post_type = $dolly->post_type;
+                    $dollyLesson = [];
+//                    add_post_meta($lesson_id, '_sfwd-lessons', $this->create_sfwd_lesson($lesson_id, $dollyLesson, $new_lesson_meta));
 
-        //Save to database
-
-        if ($course_id = wp_insert_post($course->get_columns())) {
-            echo "{$request->input('course-title')} course created<br />";
-
-            // Post meta for the course
-            $this->save_course_meta($course_id, $request, $dolly);
-
-            // Once the main course is saved, process the lesson
-            for ($i = 0; $i < count($lessonName); $i++) {
-                // Save the lessons to swfd-lesson post type
-                $lesson = new Posts;
-                $dollyLesson = new Posts;
-                $dollyLesson->find($lessonID[$i]);
-
-                $lesson->post_title = $lessonName[$i];
-                $lesson->post_author = get_current_user_id();
-                $lesson->post_content = $dollyLesson->post_content;
-                $lesson->post_excerpt = $dollyLesson->post_excerpt;
-                $lesson->post_status = "publish";
-                $lesson->post_type = $dollyLesson->post_type;
-
-                //Save Lesson to database
-                if ( $arrLessons[] = $lesson_id = wp_insert_post($lesson->get_columns()) ) {
-                    //Post meta
-
-                    //Create ld_course_steps meta
-                    $this->save_lesson_meta($lesson_id, $course_id, $request, $dollyLesson);
-
-                    echo "{$lessonName[$i]} lessons created<br />";
-
-                    add_post_meta($lesson_id, 'ld_course_steps', $this->create_ld_course_steps($arrLessons));
-                } else {
-                    echo "{$lessonName[$i]} was not created";
-                }
-            }
-
-            //Create Course Steps post meta
-            add_post_meta($course_id, 'ld_course_steps', $this->create_ld_course_steps($arrLessons));
-
-            //Save the relationship as post meta
-            add_post_meta($course_id, 'created-from-one-click', true);
+        foreach($lessonIds as $lesson_id) {
+            echo $lesson_id;
+            var_dump($this->create_sfwd_lesson($lesson_id, $dollyLesson, $new_lesson_meta));
 
         }
 
-        _redirect('https://coursesstaging3.writerscentre.com.au/wp-admin/admin.php?page=one-click-classroom-setup', []);
+//
+//                    echo "{$lessonName[$i]} lessons created<br />";
+//
+//                    add_post_meta($lesson_id, 'ld_course_steps', $this->create_ld_course_steps($arrLessons));
+//                } else {
+//                    echo "{$lessonName[$i]} was not created";
+//                }
+//            }
+//
+//            //Create Course Steps post meta
+//            add_post_meta($course_id, 'ld_course_steps', $this->create_ld_course_steps($arrLessons));
+//
+//            //Save the relationship as post meta
+//            add_post_meta($course_id, 'created-from-one-click', true);
+//
+//        }
+//
+//        _redirect('https://coursesstaging3.writerscentre.com.au/wp-admin/admin.php?page=one-click-classroom-setup', []);
 
     }
 
@@ -137,14 +157,11 @@ class ClassroomController extends CoreController
      */
     public function save_lesson_meta($lesson_id, $course_id, $request, $dollyLesson)
     {
+
+        echo "save {$lesson_id} meta<br />";
         add_post_meta($lesson_id, 'ld_course_' . $course_id, $course_id);
 
-        $new_lesson_meta = [
-            "sfwd-lessons_visible_after_specific_date" =>
-        ];
 
-
-        add_post_meta($lesson_id, '_sfwd-lessons', $this->create_sfwd_lesson($lesson_id, $dollyLesson));
         $this->duplicate_lesson_meta($lesson_id, $dollyLesson);
     }
 
@@ -267,6 +284,8 @@ class ClassroomController extends CoreController
                 $lesson_meta[$key] = (isset($new_lesson_meta[$key])) ? $new_lesson_meta[$key] : $lesson_meta[$key];
             }
         }
+
+        echo "Created sfwd-lesson {$lesson_id}";
 
 
         return $lesson_meta;
